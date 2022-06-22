825231aec8edce48225484fd101025ae169f6496
hsk99 <3024186605@qq.com>
2022-06-22 14:54:14 +0800

    1、优化 页面展示
    2、优化 js url拼接
    
    3、RedisQueue 增加 key 前缀

--- config/plugin/webman/redis-queue/redis.php
+++ config/plugin/webman/redis-queue/redis.php
@@ -1,21 +1,30 @@
 <?php
+
+if (is_file(config_path() . '/app.php')) {
+    $app = include config_path() . '/app.php';
+} else {
+    $app = [];
+}
+
 return [
     'statistic' => [
         'host' => 'redis://127.0.0.1:6379',
         'options' => [
-            'auth'          => 'hsk99',   // 密码，字符串类型，可选参数
-            'db'            => 0,         // 数据库
-            'max_attempts'  => 5,         // 消费失败后，重试次数
-            'retry_seconds' => 5,         // 重试间隔，单位秒
+            'auth'          => 'hsk99',                                            // 密码，字符串类型，可选参数
+            'db'            => 0,                                                  // 数据库
+            'prefix'        => ($app['project'] ?? 'transfer-statistics') . ':',   // key 前缀
+            'max_attempts'  => 5,                                                  // 消费失败后，重试次数
+            'retry_seconds' => 5,                                                  // 重试间隔，单位秒
         ]
     ],
     'data' => [
         'host' => 'redis://127.0.0.1:6379',
         'options' => [
-            'auth'          => 'hsk99',   // 密码，字符串类型，可选参数
-            'db'            => 0,         // 数据库
-            'max_attempts'  => 5,         // 消费失败后，重试次数
-            'retry_seconds' => 5,         // 重试间隔，单位秒
+            'auth'          => 'hsk99',                                            // 密码，字符串类型，可选参数
+            'db'            => 0,                                                  // 数据库
+            'prefix'        => ($app['project'] ?? 'transfer-statistics') . ':',   // key 前缀
+            'max_attempts'  => 5,                                                  // 消费失败后，重试次数
+            'retry_seconds' => 5,                                                  // 重试间隔，单位秒
         ]
     ],
 ];
